14ce89b5015c3ab18b23bd8b7f3f3bc709ef12e6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-16 20:04:38 +0700

    Babel config, api key in ignored different file

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import ReactDOM from 'react-dom';
 import config from '../config.js';
 
 const API_KEY = config.YoutubeAPIKey;
+
+const App = () => {
+	return <div>Wadup</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
