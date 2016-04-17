14201ff6326b23e90a70503549482c48cd222ea1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-17 20:40:12 +0700

    First search bar advances

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import config from '../config.js';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = config.YoutubeAPIKey;
 
 const App = () => {
-	return <div>Wadup</div>;
+	return(
+		<div>
+		<SearchBar />
+		</div>);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
