ccd1ae805584c388fbca13f69c8350bf8d6e853b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-17 20:55:42 +0700

    Controlled search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,8 +9,9 @@ class SearchBar extends Component {
     render() {
         return (
         	<div>
-        		<input onChange={this.onInputChange}/>
-        		Value of the input: {this.state.term}
+        		<input
+        			value={this.state.term}
+        			onChange={this.onInputChange}/>
         	</div>
         	);
     }
