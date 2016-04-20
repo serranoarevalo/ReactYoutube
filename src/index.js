2455e325c49d854ea1fc4b2c7613c4212a4a1347
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-20 17:48:59 +0700

    Youtube search added to the state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import config from '../config.js';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = config.YoutubeAPIKey;
 
-const App = () => {
-	return(
-		<div>
-		<SearchBar />
-		</div>);
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { 
+			videos: []
+		};
+
+		YTSearch({key: API_KEY, term: 'surf'}, (data) => {
+			this.setState({ videos })
+		});
+	}
+	render(){
+		return(
+				<div>
+					<SearchBar />
+				</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
