56a2c9a5b072d9189ba2d731c3a398af8369e5aa
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-21 09:31:25 +0700

    Displaying video title and thumbnail

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -5,8 +5,22 @@ class VideoListItem extends React.Component {
         super(props);
     }
     render() {
+
+    	const imageUrl = this.props.video.snippet.thumbnails.default.url;
+
         return(
-        	<li>Video</li>
+        	<li className="list-group-item">
+        		<div className="video-list media">
+        			<div className="media-left">
+        				<img className="media-object" src={imageUrl}/>
+        			</div>
+        			<div className="media-body">
+        				<div className="media-heading">
+        				{this.props.video.snippet.title}
+        				</div>
+        			</div>
+        		</div>
+        	</li>
         );
     }
 }
