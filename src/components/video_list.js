83f0b5308d5b845598c6328e5953f493fc9dbbe5
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-21 07:44:45 +0700

    Added react key to video

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,11 +4,10 @@ import VideoListItem from './video_list_item'
 class VideoList extends React.Component {
 	constructor(props) {
 		super(props);
-		this.displayName = 'VideoList';
 	}
 	render() {
-		const videoItems = props.videos.map((video) => {
-			return <VideoListItem video={video} />
+		const videoItems = this.props.videos.map((video) => {
+			return <VideoListItem key={video.etag} video={video} />
 		});
 		return (
 			<ul className="col-md-4 list-group">
