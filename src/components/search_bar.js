06a311fe5ecdffa91e003577b1136b630cf07aaa
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2016-04-22 10:18:13 +0700

    Automatically search when input change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,13 +11,14 @@ class SearchBar extends Component {
         	<div className="search-bar">
         		<input
         			value={this.state.term}
-        			onChange={this.onInputChange}/>
+        			onChange={event => this.onInputChange(event.target.value)}/>
         	</div>
         	);
     }
 
-    onInputChange = (event) => {
-    	this.setState({term: event.target.value});
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -19,7 +19,11 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term: 'surf'}, (videos) => {
+		this.videoSearch('surboards');
+	}
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({ 
 				videos:videos,
 				selectedVideo: videos[0]
@@ -29,7 +33,7 @@ class App extends Component {
 	render(){
 		return(
 				<div>
-					<SearchBar />
+					<SearchBar onSearchTermChange={ term => this.videoSearch(term)}/>
 					<VideoDetail video={this.state.selectedVideo}/>
 					<VideoList 
 						videos={this.state.videos}
